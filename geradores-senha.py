aae67ae3c0acf13e87b79e7cb6e5485a52f30ce2
Guilherme de Andrade Lima <144403720+GuilhermeAmil@users.noreply.github.com>
2026-04-20 10:41:37 -0300

    Refactor password generator with improved input handling

--- geradores-senha.py
+++ geradores-senha.py
@@ -14,18 +14,23 @@ def gerar_senha(tamanho):
         senha += random.choice(caracteres)
     return senha
 
-tamanho = ""
-try:#Try é um loop de segurança, impedindo que pareça que o programa "quebrou" quando o usuario digitar algo fora do escopo.
-    tamanho = int(input('Digite o tamanho que voce deseja sua senha: ')) #Se o usuario digitar um numero, o codigo continua aqui
-    qtd = ""
-    try:
-        qtd = int(input('Digite a quantidade de senhas que voce deseja: '))
-    except:
-        print('Digite um numero inteiro')
-    print(f'Gerando {qtd} senhas: ')
+while True:
+    try:#Try é um loop de segurança, impedindo que pareça que o programa "quebrou" quando o usuario digitar algo fora do escopo.
+        tamanho = int(input('Digite o tamanho que voce deseja sua senha: ')) #Se o usuario digitar um numero, o codigo continua aqui
+        qtd = ""
+        while True:
+            try:
+                qtd = int(input('Digite a quantidade de senhas que voce deseja: '))
+                print(f'Gerando {qtd} senhas: ')
+                for i in range(qtd):
+                    nova_senha = gerar_senha(tamanho)
+                    print(f'{i+1}ª senha: {nova_senha}')
+                break
+            except:
+                print('Ops! Digite um numero inteiro...')
 
-    for i in range(qtd):
-        nova_senha = gerar_senha(tamanho)
-        print(f'{i+1}ª senha: {nova_senha}')
-except ValueError:
-    print('Ops!. Você precisa digitar um numero inteiro (Ex: 12)')#Se o usuario digitar algo que nao seja um numero, o codigo Python pula pra cá
+            
+        break
+    except ValueError:
+        print('Ops!. Você precisa digitar um numero inteiro (Ex: 12)')#Se o usuario digitar algo que nao seja um numero, o codigo Python pula pra cá
+
